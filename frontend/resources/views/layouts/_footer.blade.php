304c8413535e91ea4954af0085c5abe7da03feb3
name-T <namet117@163.com>
2018-03-16 15:49:53 +0800

    compress js & css

--- frontend/resources/views/layouts/_footer.blade.php
+++ frontend/resources/views/layouts/_footer.blade.php
@@ -1,10 +1,13 @@
 <footer class="footer">
     <div class="container">
         <p class="pull-left">
-            Auth: <a href="mailto:namet@wangegou.cn">name-T</a>
+            Auth: <a href="mailto:namet117@163.com">name-T</a>
         </p>
         <p class="pull-right">
             <a href="http://www.miitbeian.gov.cn" target="_blank">粤ICP备17142456号-1</a>
         </p>
+        <div class="hidden">
+            <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1273145933'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1273145933' type='text/javascript'%3E%3C/script%3E"));</script>
+        </div>
     </div>
 </footer>
