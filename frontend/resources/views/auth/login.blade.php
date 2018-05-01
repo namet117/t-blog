ce40813b1585fe217375eabaefa3691de6133f7a
name-T <namet117@163.com>
2018-05-01 22:19:29 +0800

    login-3

--- frontend/resources/views/auth/login.blade.php
+++ frontend/resources/views/auth/login.blade.php
@@ -4,13 +4,15 @@
     <div class="col-md-8 col-md-offset-2">
         <div class="panel panel-default">
             <div class="panel-heading">可使用以下方式登陆</div>
-            <div class="panel-body">
-                <span>QQ</span>
-                <span>微信</span>
-                <span>微博</span>
-                <span>Github</span>
+            <div class="panel-body" id="user-login">
+                <img src="http://file.namet.xyz/material/third-party.png" alt="">
             </div>
         </div>
     </div>
 </div>
+<style media="screen">
+/* #qq {
+    background: url('http://file.namet.xyz/material/third-party.png');
+} */
+</style>
 @endsection
