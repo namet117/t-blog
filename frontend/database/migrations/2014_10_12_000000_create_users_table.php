73543f7280e19b2e02275b5ca6ee9613b3dd8e09
nameT <namet117@163.com>
2018-05-18 19:02:00 +0800

    update migrate files

--- frontend/database/migrations/2014_10_12_000000_create_users_table.php
+++ frontend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,10 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('username', 50)->comment('用户名')->unique();
             $table->string('email', 100)->comment('邮箱')->default('')->unique();
             $table->string('telephone', 20)->comment('手机号')->default('')->unique();
-            $table->string('avatar', 100)->comment('头像')->nullable();
+            $table->string('avatar', 100)->comment('头像')->default('');
             $table->rememberToken();
             // TODO delete
-            $table->string('api_token')->comment('API TOKEN')->nullable();
+            $table->string('api_token')->comment('API TOKEN')->default('');
             $table->tinyInteger('is_admin')->comment('是否管理员')->default(0);
             $table->timestamps();
         });
