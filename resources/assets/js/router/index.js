1ce5d0e4498b478e59c71af6ca17a1ceebdae398
name-T <namet117@163.com>
2018-06-09 22:53:45 +0800

    update packages

--- resources/assets/js/router/index.js
+++ resources/assets/js/router/index.js
@@ -1,49 +1,21 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import router from './router';
+import { Message } from 'element-ui';
+import NProgress from 'nprogress'; // progress bar
+import 'nprogress/nprogress.css';// progress bar style
+import { title } from '../utils/index' // getToken from cookie
 
-Vue.use(Router);
 
-const routes = [
-    // {
-    //     path: '/*',
-    //     name: 'error-404',
-    //     meta: {
-    //         title: '404-页面不存在'
-    //     },
-    //     component: resolve => { require(['../views/error-page/404.vue'], resolve); }
-    // },
-    {
-        path: '/login',
-        name: 'admin-log',
-        component: resolve => require(['../views/admin/home/login'], resolve),
-        hidden: true
-    },
-    // {
-    //     path: '/',
-    //     component: resolve => require(['../components/home/home'], resolve),
-    //     hidden: true
-    // },
-    // {
-    //     path: '/',
-    //     component: resolve => require(['../components/home/home'], resolve),
-    //     name: '焦点图管理',
-    //     iconCls: 'el-icon-picture',
-    //     hidden: false,
-    //     children: [
-    //         {
-    //             name: '登录焦点图',
-    //             path: '/focus/index',
-    //             component: resolve => require(['../components/focus/focus-index'], resolve)
-    //          },
-    //         {
-    //             name: '首页焦点图',
-    //             path: '/focus/login',
-    //             component: resolve => require(['../components/focus/focus-login'], resolve)
-    //         }
-    //     ]
-    // },
-];
+NProgress.configure({ showSpinner: false })// NProgress Configuration
 
-const router = new Router({ routes });
+router.beforeEach((to, from, next) => {
+    NProgress.start(); // start progress bar
+    title(to.meta.title);
+    next();
+})
 
-export default router;
+router.afterEach(() => {
+    NProgress.done() // finish progress bar
+})
+
+export default router
