2d472c8366da76c593367e746309650c7d9bd0aa
nameT <namet117@163.com>
2018-08-20 16:42:22 +0800

    remove vue-related

--- resources/assets/js/icon/index.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue';
-import FontAwesome from '@/components/FontAwesome';
-
-// register icon globally
-Vue.component('fa-icon', FontAwesome);
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,29 +1,4 @@
 let mix = require('laravel-mix');
-const path = require('path');
-const cleanWebpackPlugin = require('clean-webpack-plugin');
-function resolve (dir) {
-  return path.join(__dirname, '.', dir)
-}
-mix.webpackConfig({
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['.js', '.vue', '.json'],
-        alias: {
-          '@': resolve('resources/assets/js'),
-        }
-    },
-    output: {
-        publicPath: "/",
-        chunkFilename: 'js/[name].[hash].chunk.js'
-    },
-    plugins: [
-        new cleanWebpackPlugin(['js/*'], {
-            root: path.resolve(__dirname, './public/'),
-            dry: false,
-            verbose: true,
-        }),
-    ]
-});
 
 // 停用通知
 mix.disableNotifications();
@@ -31,5 +6,4 @@ mix.disableNotifications();
 mix
 .js('resources/assets/js/app.js', 'public/js')
 .sass('resources/assets/sass/app.scss', 'public/css')
-.js('resources/assets/js/admin.js', 'public/js')
 .version();
