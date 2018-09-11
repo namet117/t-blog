a3dc7b93ba40cec17fa0480a68da40d9320de516
name-T <namet117@163.com>
2018-09-12 00:31:23 +0800

    fix config bug

--- config/admin.php
+++ config/admin.php
@@ -85,7 +85,7 @@ return [
     | If your page is going to be accessed via https, set it to `true`.
     |
     */
-    'secure' => config('app.env') === 'production',
+    'secure' => env('APP_ENV', 'production') === 'production',
 
     /*
     |--------------------------------------------------------------------------
