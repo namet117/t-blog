3c65d54725ce32971127a811d1eef08946102395
name-T <namet117@163.com>
2018-09-20 23:58:18 +0800

    fix bug

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -49,8 +49,3 @@ if (!function_exists('is_url')) {
         return preg_match('/^https?:\//', $url);
     }
 }
-
-function f_log($data, $append = false)
-{
-    return file_put_contents('l1.log', print_r($data, true), $append ? FILE_APPEND : 0);
-}
