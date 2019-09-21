95218bc81c0e6432028bd955751fbbffac2282b4
namet117 <namet117@163.com>
2019-09-21 14:05:17 +0800

    修改百度统计appid

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -17,13 +17,14 @@
 <div id="back-to-top">
     <i class="glyphicon glyphicon-triangle-top"></i>
 </div>
+
 <script>
 var _hmt = _hmt || [];
 (function() {
-      var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?285daa10c97c3380a0262ea7fe0c9b7b";
-        var s = document.getElementsByTagName("script")[0]; 
-          s.parentNode.insertBefore(hm, s);
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?4ef5c6918597ff60d781f4cd4a970e39";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
 })();
 </script>
 
